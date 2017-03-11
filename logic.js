0d683976e94a5db0e447ca8bd7352b601514a1c4
hstaykov <riceto.new@gmail.com>
2017-03-11 17:02:36 +0200

    Fix umlauts

--- logic.js
+++ logic.js
@@ -20,7 +20,7 @@ var testDictionary = [];
           if (word) {
               $.get(
                   "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170220T190751Z.804f007c23b914ea.50c6cef984e1cc17193a6608117fceaed88a6cfc&text=" +
-                  word + "&lang=de-en",
+                  encodeURIComponent(word) + "&lang=de-en",
                   function(data, status) {
                     var sameWord = true;
                     if(word != data.text){
@@ -203,12 +203,12 @@ var testDictionary = [];
               }
           });
 
-          allReft.on('child_added', function(data) {
+          allReft.orderByKey().on('child_added', function(data) {
 
    
           
               $("#right ul").append(
-                  '<li><span class="tab"><a target="_blank" href="http://www.linguee.de/deutsch-englisch/search?source=auto&query=' +
+                  '<li><span class="tab"><a class="list" target="_blank" href="http://www.linguee.de/deutsch-englisch/search?source=auto&query=' +
                   data.val().german + '"><img src="german.png" style="width:25px;margin-right: 15px;">' + data.val().german +
                   ' - ' + data.val().english +
                   '<img src="britain.png" style="width:25px;margin-left: 15px;"></a></span><img onclick="removeWord(\'' +
@@ -218,7 +218,7 @@ var testDictionary = [];
       }
 
       function getRandomWords(){
-        var number = 5;
+        var number = 15;
         var randomWords = [];
         for(i = 0 ; i<number ; i++){
           var index = Math.floor(Math.random() * testDictionary.length);
@@ -244,7 +244,7 @@ var testDictionary = [];
         this.startTest = function(){
           $("#btnTest").remove();
           for (var i = this.words.length - 1; i >= 0; i--) {
-             $(".testList").append('<li data-id="' + i +'" style="display: none"><p> Do you know what <a class="testWord">' + this.words[i].german +'</a> means ?<input id="testWord" ng-keyup="$event.keyCode == 13 ? testWord() : null" type="text" value="" placeholder=""></input></p></li>');
+             $(".testList").append('<li data-eng="'+this.words[i].english+' " data-id="' + i +'" style="display: none"><p><span> Do you know what <span class="bolded">' + this.words[i].german +'</span> means ?</span><input id="testWord" onkeypress="chcekAnswer(event)" type="text" value="" placeholder=""></input></p></li>');
           };
           $(".testList").find('[data-id="0"]').show();
           $(".testList").find('[data-id="0"]').addClass("active");
@@ -264,11 +264,30 @@ var testDictionary = [];
       }
   
 
-function chcekAnswer(){
+function chcekAnswer(event){
+    if(event.keyCode == 13){
+      var correct = parseInt($(".correct.score").html());
+      var notcorrect = parseInt($(".notCorrect.score").html());
+      var currentQuestion = $(".testList").children(".active");
 
-    var currentQuestion = $(".testList").children(".active");
-    
-    
+      var guess = currentQuestion.find("input").val().trim();
+      var answer = currentQuestion.data("eng").trim();
+      console.log(guess + " " + answer);
+      var show = true;
+      if(guess.toString().toUpperCase() === answer.toString().toUpperCase()){
+        currentQuestion.find("p").append("<span class='correct'>Correct</span>");
+        correct++;
+        $(".correct.score").html(correct)
+
+      }
+      else{
+        currentQuestion.find("p").append("<span class='notCorrect'>Wrong. It is <span class='bolded'>" + answer + "</span></span>")
+        notcorrect++;
+        $(".notCorrect.score").html(notcorrect);
+      }
+
+      setInterval(function(){if(show){changeQuestion(currentQuestion);show=false}},2000);
+    }
 }
 
 
@@ -278,5 +297,6 @@ function changeQuestion(currentQuestion){
     var currentId = parseInt(currentQuestion.data("id"));
     var nextId = currentId + 1;
     $(".testList").find('[data-id="'+nextId+'"]').show();
+    $(".testList").find('[data-id="'+nextId+'"]').find("input").focus();
     $(".testList").find('[data-id="'+nextId+'"]').addClass("active");
 }
